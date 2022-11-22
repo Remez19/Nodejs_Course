1f13825d70643f7ec8d960e88fdb9edf0bae6330
Remez19 <rdoneplus@gmail.com>
2022-11-22 14:55:20 +0200

    Parsing Request Bodies

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const http = require("http");
-
+const fs = require("fs");
 /*
     The requestListener function gets two args:
     requst - in type of incoming message.
     response - object to send to the request.  
 */
 
-function requsetListener(req, res) {
-  console.log(req);
-  //   console.log("remez");
-}
+// function requsetListener(req, res) {
+//   console.log(req.url, req.method, req.headers);
+//   //   res.
+//   // exit the running process
+//   // process.exit();
+//   // console.log("remez");
+// }
 
 /* 
     Takes a request listener:  
@@ -17,14 +20,52 @@ function requsetListener(req, res) {
     It returs a SERVER
 */
 
-const server = http.createServer(requsetListener);
+// const server = http.createServer(requsetListener);
 
-/* 
-    Same result diffrent approach:
-    http.createServer((req, res) =>{
-    
-    })
-*/
+// Same result diffrent approach:
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Name</title>");
+    res.write("<body>");
+    res.write(
+      '<form action="/message" method="POST"><input type="text" name="message"/><button type="sumbit">Send</button></form>'
+    );
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    /* 
+     on allows us to listen t odiffrent events.
+     the data event will be fired whenever a new chunck 
+     is ready to be read.
+    the second argument is a function that will be 
+    excuted for every data event.
+     */
+    const body = [];
+    req.on("data", (chunck) => {
+      //   console.log(chunck);
+      body.push(chunck);
+    });
+    res.on("finish", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      //   console.log(message);
+      fs.writeFileSync("./UserMessage.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write(
+    "<html><hrader><title>My Page</title></header><body><h1>Remez</h1></body></html>"
+  );
+  res.end();
+});
 
 /* 
     listen: 
