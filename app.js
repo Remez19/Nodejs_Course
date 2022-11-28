c1e0dce107566e0efea571216695209da757deae
Remez19 <rdoneplus@gmail.com>
2022-11-28 16:02:10 +0200

    Working on the Layout with Partials

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(shopRoutes);
 app.use((req, res) => {
   res.status(404).render("404", {
     pageTitle: "Page Not Found",
+    path: "",
   });
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
