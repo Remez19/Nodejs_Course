bda1fc19440756d675ab4e3a78c8d507fecaf711
Remez19 <rdoneplus@gmail.com>
2022-11-30 16:26:25 +0200

    Connecting to mogodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,23 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+/**
+ * mongoConnect - Function to Connent to mongodb server.
+ * @param {Function} callBack - the function to excute once the connent is done.
+ */
+const mongoConnect = (callBack) => {
+  MongoClient.connect(
+    // The connection string provided by mongodb website
+    "mongodb+srv://Remez:hj0mm0puhT9FUv0b@nodejscourse.7vjdhyd.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("CONNECTED TO MONGODB!");
+      callBack(client);
+    })
+    .catch((error) => {
+      // In case of error while trying to connect to the database
+      console.log(error);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
