e9ccb458c557d7941fbe64d8cb38381e025e23f4
Remez19 <rdoneplus@gmail.com>
2022-12-06 13:36:03 +0200

    Working on Cart Items & Orders

--- models/user.js
+++ models/user.js
@@ -4,15 +4,35 @@ const mongodb = require("mongodb");
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(userName, email) {
+  constructor(userName, email, cart, id) {
     this.userName = userName;
     this.email = this.email;
+    this.cart = cart; // {items: []}
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection("users").insertOne(this);
   }
+
+  addProductToCart(product) {
+    // const cartProduct = this.cart.items.findIndex((cartProduct) => {
+    //   return cartProduct._id === product._id;
+    // });
+
+    // Adding field to the product on the fly
+    // product.quantity = 1;
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
   static findUserById(userId) {
     const db = getDb();
     return db
