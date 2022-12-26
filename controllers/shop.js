10763f33ee35bea91518c511d31d96c679d3590f
Remez19 <rdoneplus@gmail.com>
2022-12-26 12:02:37 +0200

    Using Stripe in Your App

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const Product = require("../models/product");
 const User = require("../models/user");
 const Order = require("../models/order");
 const fs = require("fs");
 const path = require("path");
+const stripe = require("stripe")(process.env.StripeKey);
 
 const PDFDocument = require("pdfkit");
 
@@ -144,6 +146,107 @@ exports.postCartDeleteProduct = (req, res) => {
     .removeFromCart(productId)
     .then((result) => {
       res.redirect("/cart");
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      // Will cause node to jump to a middleware that
+      // handle errors
+      return next(error);
+    });
+};
+
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let totalSum = 0;
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      products = user.cart.items;
+      totalSum = 0;
+      products.forEach((product) => {
+        totalSum += product.productId.price * product.quantity;
+      });
+      // To create we can pass object to configure the session
+      return stripe.checkout.sessions.create({
+        // Which type of payments we accept
+        payment_method_types: ["card"],
+        // which items will be checked out
+        line_items: products.map((product) => {
+          return {
+            // All the data stripe need
+            // name: product.productId.title,
+            // description: product.productId.description,
+            // images: [],
+            // amount: product.productId.price * 100,
+            // currency: "usd",
+            price_data: {
+              currency: "usd",
+              unit_amount: product.productId.price * 100,
+              product_data: {
+                name: product.productId.title,
+                description: product.productId.description,
+                images: [],
+              },
+            },
+            quantity: product.quantity,
+          };
+        }),
+        mode: "payment",
+        // This are urls stripe will redirect the user to once the transaction
+        // is finished (success or fail)
+        success_url:
+          req.protocol + "://" + req.get("host") + "/checkout/success", // reconstructing url => http://
+        cancel_url: req.protocol + "://" + req.get("host") + "/checkout/cancel",
+      });
+    })
+    .then((session) => {
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      // Will cause node to jump to a middleware that
+      // handle errors
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  // In order to get the full data of each product in the cart we can use
+  // "populate("cart.items.productId")"
+  User.findById({ _id: req.user._id })
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((item) => {
+        return {
+          // mongoose gives us a special field clled "_doc"
+          // with the "..." operator we can pull all the data in the
+          // object
+          productData: { ...item.productId._doc },
+          quantity: item.quantity,
+        };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect("/orders");
     })
     .catch((err) => {
       const error = new Error(err);
